82ae4748f2fa02b3cc2f978976fae8ae454a7a14
Thomas <tsanavia@gmail.com>
2021-03-16 21:54:15 +0100

    Rest Api complete

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudent(){
+
+        List<Student> students = Arrays.asList(
+                new Student(1L, "Thomas Sanavia", "tsanavia@gmail.com", Gender.MALE),
+                new Student(2L, "Thomas Zin", "tsanavia@gmail.com", Gender.MALE)
+        );
+
+        return students;
+    }
+}
