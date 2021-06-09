26657e88c708913db1b07600e3e92b156409460b
Thomas <tsanavia@gmail.com>
2021-06-09 17:53:06 +0200

    change test result

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,9 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Description("Test di prova")
 	@Test
 	void contextLoads() {
-		Assertions.fail("Fail!");
+		Assertions.assertTrue(true);
 	}
 
 }
